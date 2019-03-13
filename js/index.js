05fd5e15e381b9f5ec0dee89d23305a7c0ca3558
Latwan Wesley <wesleylatwan22@gmail.com>
2019-03-13 16:35:38 -0600

    Still need to complete this project

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const logoHeading = document.querySelectorAll('h1'); 
+console.log(logoHeading); 
+logoHeading[0].style.color='crimson'; 
+
+const button = document.querySelector('')
